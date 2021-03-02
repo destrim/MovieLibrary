5611d2857c49d60656ecd2df1a228221a069ea8b
destrim <slawek.potoczek@gmail.com>
2021-03-02 09:58:06 +0100

    JSON and file handling class refactor

--- src/main/java/com/destrim/util/FileHandling.java
+++ src/main/java/com/destrim/util/FileHandling.java
@@ -1,8 +1,10 @@
 package com.destrim.util;
 
 import com.destrim.model.Movie;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
@@ -10,60 +12,15 @@ import java.nio.file.Path;
 import java.util.*;
 
 public class FileHandling {
+
     public static void exportToFile(List<Movie> movies, String fileName) throws IOException {
-        JSONObject obj = new JSONObject();
-        int i = 1;
-        for (Movie movie : movies) {
-            Map<String, String> m = new LinkedHashMap<>(5);
-            m.put("Title", movie.getTitle());
-            m.put("Released", movie.getReleased());
-            m.put("Genre", movie.getGenre());
-            m.put("Plot", movie.getPlot());
-            m.put("imdbRating", movie.getImdbRating());
-
-            obj.put("movieNo" + i, m);
-            i++;
-        }
-
-        PrintWriter pw = new PrintWriter(fileName + ".json");
-        pw.write(obj.toString());  // TODO json formatting
-        pw.flush();
-        pw.close();
+        JSONArray arr = getJsonArrayFromMovies(movies);
+        saveJsonArrayToFile(fileName, arr);
     }
 
-    public static ArrayList<Movie> importFromFile(String fileName) throws IOException {
-        ArrayList<Movie> movies = new ArrayList<>();
-
-        Path path = Path.of(fileName + ".json");
-        String jsonFromFile = Files.readString(path);
-
-        String findStr = "movieNo";
-        int lastIndex = 0;
-        int count = 0;
-        while (lastIndex != -1) {
-            lastIndex = jsonFromFile.indexOf(findStr, lastIndex);
-            if (lastIndex != -1) {
-                count++;
-                lastIndex += findStr.length();
-            }
-        }
-
-        for (int i = 1; i <= count; i++) {
-            JSONObject obj = new JSONObject(jsonFromFile);
-            String countMovies = Integer.toString(i);
-            obj = obj.getJSONObject(findStr + countMovies);
-
-            Movie movie = new Movie(
-                    obj.getString("Title"),
-                    obj.getString("Released"),
-                    obj.getString("Genre"),
-                    obj.getString("Plot"),
-                    obj.getString("imdbRating")
-            );
-
-            movies.add(movie);
-        }
-        return movies;
+    public static List<Movie> importFromFile(String fileName) throws IOException {
+        JSONArray arr = parseJsonArrayFromFile(fileName);
+        return getMoviesFromJsonArray(arr);
     }
 
     public static Optional<String> importApikey() {
@@ -74,5 +31,60 @@ public class FileHandling {
         } catch (IOException e) {
             return Optional.empty();
         }
+    }
+
+    private static JSONArray getJsonArrayFromMovies(List<Movie> movies) {
+        JSONArray arr = new JSONArray();
+
+        for (Movie movie : movies) {
+            JSONObject obj = getJsonObjectFromMovie(movie);
+            arr.put(obj);
+        }
+        return arr;
+    }
+
+    private static void saveJsonArrayToFile(String fileName, JSONArray arr) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(fileName + ".json");
+        pw.write(arr.toString(3));
+        pw.flush();
+        pw.close();
+    }
+
+    private static JSONArray parseJsonArrayFromFile(String fileName) throws IOException {
+        Path path = Path.of(fileName + ".json");
+        String jsonFromFile = Files.readString(path);
+        return new JSONArray(jsonFromFile);
+    }
+
+    private static List<Movie> getMoviesFromJsonArray(JSONArray arr) {
+        List<Movie> movies = new ArrayList<>();
+
+        for (int i = 0; i < arr.length(); i++) {
+            Movie movie = getMovieFromJsonObject(arr, i);
+            movies.add(movie);
+        }
+        return movies;
+    }
+
+    private static JSONObject getJsonObjectFromMovie(Movie movie) {
+        Map<String, String> m = new HashMap<>(5);
+        m.put("Title", movie.getTitle());
+        m.put("Released", movie.getReleased());
+        m.put("Genre", movie.getGenre());
+        m.put("Plot", movie.getPlot());
+        m.put("imdbRating", movie.getImdbRating());
+        return new JSONObject(m);
+    }
+
+    private static Movie getMovieFromJsonObject(JSONArray arr, int i) {
+        JSONObject obj = arr.getJSONObject(i);
+
+        return new Movie(
+                obj.getString("Title"),
+                obj.getString("Released"),
+                obj.getString("Genre"),
+                obj.getString("Plot"),
+                obj.getString("imdbRating")
+        );
     }
 }
