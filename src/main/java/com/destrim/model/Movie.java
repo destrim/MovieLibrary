3b1fbb0664c7b030153d5e1468c39f85e5ace210
destrim <slawek.potoczek@gmail.com>
2021-03-23 23:59:25 +0100

    added builder and separated model from presistence

--- src/main/java/com/destrim/model/Movie.java
+++ src/main/java/com/destrim/model/Movie.java
@@ -1,9 +1,6 @@
 package com.destrim.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,6 +11,7 @@ import javax.persistence.Id;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Movie {
 
